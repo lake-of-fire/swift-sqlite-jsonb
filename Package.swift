f45ff59240f78f786a2b08f2121ce46582e0a6ba
Alex Ehlke <alex.ehlke@gmail.com>
2025-12-28 11:21:11 -0500

    Update platform versions for macOS and iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SQLiteJSONB",
     platforms: [
-        .macOS(.v12), .iOS(.v14)
+        .macOS(.v13), .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
